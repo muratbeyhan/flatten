1f32f91361142207d2c5e6efc4b75c5a22bd972d
Murat Beyhan <44893123+muratbeyhan@users.noreply.github.com>
2024-04-18 17:24:00 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -11,3 +11,18 @@ nested_list = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 
 flattened_list = deep_flatten_list(nested_list)
 print(flattened_list)
+
+
+
+def reverse_nested_list(nested_lst):
+    reversed_list = []
+    for item in reversed(nested_lst):
+        if isinstance(item, type([])):
+            reversed_list.append(reverse_nested_list(item))
+        else:
+            reversed_list.append(item)
+    return reversed_list
+
+nested_list = [[1, 2], [3, 4], [5, 6, 7]]
+reversed_list = reverse_nested_list(nested_list)
+print(reversed_list)
